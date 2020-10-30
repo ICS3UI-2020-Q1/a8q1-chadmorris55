0c3e82a88e943cb1c0f7ddea0533294d27073756
chadmorris55 <71134162+chadmorris55@users.noreply.github.com>
2020-10-30 12:47:54 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,13 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
+  JLabel firstLabel;
+
+  JTextField firstInput;
   
+  JButton startButton;
+
+  JPanel mainPanel;
 
 
   // Method to assemble our GUI
@@ -18,16 +24,48 @@ public class Main implements Runnable, ActionListener{
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
+
+    //initialize the main panel 
+    mainPanel = new JPanel();
+    //manual layout
+    mainPanel.setLayout(null);
+    //create the text box
+    firstInput = new JTextField();
+    firstInput.setBounds(100,100,600,20);
+
+    //create the text field
+    firstLabel = new JLabel("");
+    firstLabel.setBounds(100,250,600,20);
+    
+    //create the button
+    startButton = new JButton("Say Hello");
+    startButton.setBounds(320,400,150,20);
+
+    //set action command
+    startButton.setActionCommand("start");
+
+    //add action ActionListener
+    startButton.addActionListener(this);
+
+    //add things to the main panel 
+    mainPanel.add(firstInput);
+    mainPanel.add(firstLabel);
+    mainPanel.add(startButton);
+    //add main panel to screen 
+    frame.add(mainPanel);
  
     
 
   }
-
   // method called when a button is pressed
   public void actionPerformed(ActionEvent e){
     // get the command from the action
     String command = e.getActionCommand();
-
+    //goes when button is pressed
+    if(command.equals("start")){
+      String name = firstInput.getText();
+      firstLabel.setText("hello " + name);
+    }
   }
 
   // Main method to start our program
